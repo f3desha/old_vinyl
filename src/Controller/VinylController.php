523473b5348750787002f491942fae134777aebc
Olga Kuzmenko <germioneys@gmail.com>
2023-03-02 22:32:49 +0200

    First controller

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController {
+    #[Route('/')]
+    public function homepage(): Response {
+        return new Response('Voila!');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-',' ', $slug))->title(true);
+        } else {
+            $title = 'Genre: All genres';
+        }
+        return new Response($title);
+    }
+
+}
